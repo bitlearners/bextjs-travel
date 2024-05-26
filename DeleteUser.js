d8f68fefde4bac248405b336c48b4fb833cc6c97
Bitlearners <parateabhishek404@gmail.com>
2024-05-26 12:40:29 +0530

    Update DeleteUser.js
    this is working

--- DeleteUser.js
+++ DeleteUser.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function DeleteProduct(props) {
   const router =useRouter();
 
-  const =abhi;
+  
 
   const deleteRecord = async () => {
     let response = await fetch(`/api/product/${props.id}`, {
